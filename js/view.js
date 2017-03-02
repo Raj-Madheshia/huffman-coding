761ecdde88015a3dee9f9b49bc749f3c6a59b614
kb-studios <2015kaushal.bhogale@ves.ac.in>
2017-03-02 20:48:02 +0530

    store data into object and display in console

--- js/view.js
+++ js/view.js
@@ -12,7 +12,7 @@ function displayContent() {
 
   huffArr = huffString.split(",").map(Number);
   var j = huffArrStore.length;
-  console.log(j);
+  //console.log(j);
   huffArrStore[0] = [];
   for(var i=0; i < huffArr.length; i++){
     var huffobj = new huffClass(
@@ -26,14 +26,29 @@ function displayContent() {
   huffArrStore[j][huffArrStore[j].length-2].value=0;
   huffArrStore[j][huffArrStore[j].length-1].value=1;
 
+  //console.log(huffArrStore);
+
   while (huffArr.length!=1) {
-    huffArr[huffArr.length-2] = huffArr[huffArr.length-1] + huffArr[huffArr.length-2];
+    var l = huffArr.length;
+
+    huffArr[l-2] = huffArr[l-1] + huffArr[l-2];
     huffArr.pop();
+
     j = huffArrStore.length;
     huffArrStore[j] = [];
-    console.log(j);
+
+    var calcProb = (
+      huffArrStore[j-1][huffArrStore[j-1].length-2].probability
+      + huffArrStore[j-1][huffArrStore[j-1].length-1].probability
+    );
+
+    var calcName = (
+      huffArrStore[j-1][l-2].name + huffArrStore[j-1][l-1].name
+    );
+
+    //console.log(j);
     for(var i=0; i < huffArr.length; i++){
-      var huffobj = new huffClass(
+       var huffobj = new huffClass(
         huffArr[i],
         'x' + i,
         -1
@@ -41,20 +56,26 @@ function displayContent() {
       huffArrStore[j][i] = huffobj;
     }
 
-    huffArrStore[j][huffArrStore[j].length-2].value=0;
-    huffArrStore[j][huffArrStore[j].length-1].value=1;
+    huffArrStore[j][huffArrStore[j].length-1].probability = calcProb;
+    huffArrStore[j][huffArrStore[j].length-1].name = calcName;
+
+    if(huffArrStore[j].length!=1){
+      huffArrStore[j][huffArrStore[j].length-2].value=0;
+      huffArrStore[j][huffArrStore[j].length-1].value=1;
+    }
+
   }
 
   console.log(huffArrStore);
 
-  // var output = "";
-  //
-  // for (huffarrItr of huffArrStore) {
-  //   for (huffEle of huffarrItr) {
-  //     output += huffEle + " ";
-  //   }
-  //   output += "<br>"
-  // }
-  //
-  // document.getElementById("output").innerHTML = output;
+  var output = "";
+
+  for (huffarrItr of huffArrStore) {
+    for (huffEle of huffarrItr) {
+      output += huffEle.probability + " ";
+    }
+    output += "<br>"
+  }
+
+  document.getElementById("output").innerHTML = output;
 }
