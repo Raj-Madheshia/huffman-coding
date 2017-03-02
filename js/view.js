27e196cab8775a5db9301b3f0afe079342507ad9
kb-studios <2015kaushal.bhogale@ves.ac.in>
2017-03-02 07:20:27 +0530

    make object and send it over [ error exists]

--- js/view.js
+++ js/view.js
@@ -1,24 +1,60 @@
+function huffClass(probability, name, value) {
+  this.probability = probability;
+  this.name = name;
+  this.value = value;
+};
+
 function displayContent() {
   var huffString = document.getElementById("huffString").value;
+
   var huffArr = [];
+  var huffArrStore = [];
+
   huffArr = huffString.split(",").map(Number);
-  huffArrStore = [[]];
-  huffArrStore[huffArrStore.length-1] = huffArr.slice();
+  var j = huffArrStore.length;
+  console.log(j);
+  huffArrStore[0] = [];
+  for(var i=0; i < huffArr.length; i++){
+    var huffobj = new huffClass(
+      huffArr[i],
+      'x' + i,
+      -1
+    );
+    huffArrStore[0][i] = huffobj;
+  }
+
+  huffArrStore[j][huffArrStore[j].length-2].value=0;
+  huffArrStore[j][huffArrStore[j].length-1].value=1;
 
   while (huffArr.length!=1) {
     huffArr[huffArr.length-2] = huffArr[huffArr.length-1] + huffArr[huffArr.length-2];
     huffArr.pop();
-    huffArrStore[huffArrStore.length] = huffArr.slice();
-  }
-
-  var output = "";
-
-  for (huffarrItr of huffArrStore) {
-    for (huffEle of huffarrItr) {
-      output += huffEle + " ";
+    j = huffArrStore.length;
+    huffArrStore[j] = [];
+    console.log(j);
+    for(var i=0; i < huffArr.length; i++){
+      var huffobj = new huffClass(
+        huffArr[i],
+        'x' + i,
+        -1
+      );
+      huffArrStore[j][i] = huffobj;
     }
-    output += "<br>"
+
+    huffArrStore[j][huffArrStore[j].length-2].value=0;
+    huffArrStore[j][huffArrStore[j].length-1].value=1;
   }
 
-  document.getElementById("output").innerHTML = output;
+  console.log(huffArrStore);
+
+  // var output = "";
+  //
+  // for (huffarrItr of huffArrStore) {
+  //   for (huffEle of huffarrItr) {
+  //     output += huffEle + " ";
+  //   }
+  //   output += "<br>"
+  // }
+  //
+  // document.getElementById("output").innerHTML = output;
 }
