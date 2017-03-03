447672316737e8c5797d4ebd2f698c40adcd21ce
kb-studios <2015kaushal.bhogale@ves.ac.in>
2017-03-03 06:17:15 +0530

    Refactor the code and add documentation

--- js/view.js
+++ js/view.js
@@ -1,18 +1,60 @@
+/*****************************************************
+          HUFFMAN CODE IN JAVASCRIPT
+
+- A javascript of huffman coding
+- Makes use of dynamic multi-dimensional array
+  to store the tree structure
+
+- Current Version (alpha 1.0.5)
+
+*****************************************************/
+
+//Create a prototype for the huffman object
 function huffClass(probability, name, value) {
   this.probability = probability;
   this.name = name;
   this.value = value;
 };
 
-function displayContent() {
+//Global variables
+var huffArr;
+var huffArrStore;
+
+function displayTree(){
+
+  //Display the output to html
+  var output = "";
+
+  for (huffarrItr of huffArrStore) {
+    for (huffEle of huffarrItr) {
+      output += huffEle.probability + " ";
+    }
+    output += "<br>"
+  }
+
+  document.getElementById("output").innerHTML = output;
+
+}
+
+function calcCode(){
+
+  //todo [parse the tree]
+}
+
+function mainCall() {
+
+  //Get input String
   var huffString = document.getElementById("huffString").value;
 
-  var huffArr = [];
-  var huffArrStore = [];
+  //Initialise the arrays
+  huffArr = [];
+  huffArrStore = [];
 
+  //Split string to array
   huffArr = huffString.split(",").map(Number);
   var j = huffArrStore.length;
-  //console.log(j);
+
+  //Store first array to the Array-Store
   huffArrStore[0] = [];
   for(var i=0; i < huffArr.length; i++){
     var huffobj = new huffClass(
@@ -23,11 +65,14 @@ function displayContent() {
     huffArrStore[0][i] = huffobj;
   }
 
+  //Assign values of 0 and 1 to last two elements
   huffArrStore[j][huffArrStore[j].length-2].value=0;
   huffArrStore[j][huffArrStore[j].length-1].value=1;
 
+  /* Test */
   //console.log(huffArrStore);
 
+  /*******  Huffman Code generation Logic  ********************/
   while (huffArr.length!=1) {
     var l = huffArr.length;
 
@@ -54,7 +99,7 @@ function displayContent() {
         -1
       );
       huffArrStore[j][i] = huffobj;
-    }
+    }//End of for
 
     huffArrStore[j][huffArrStore[j].length-1].probability = calcProb;
     huffArrStore[j][huffArrStore[j].length-1].name = calcName;
@@ -64,18 +109,11 @@ function displayContent() {
       huffArrStore[j][huffArrStore[j].length-1].value=1;
     }
 
-  }
+  }//End of while
+  /******************************************************************/
 
   console.log(huffArrStore);
 
-  var output = "";
+  displayTree();
 
-  for (huffarrItr of huffArrStore) {
-    for (huffEle of huffarrItr) {
-      output += huffEle.probability + " ";
-    }
-    output += "<br>"
-  }
-
-  document.getElementById("output").innerHTML = output;
-}
+}//End of mainCall()
