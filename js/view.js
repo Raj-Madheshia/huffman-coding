52c0591b352f582c1c9e0ee85d406bc9fc38612e
kb-studios <2015kaushal.bhogale@ves.ac.in>
2017-03-06 22:13:53 +0530

    Add more styling

--- js/view.js
+++ js/view.js
@@ -10,36 +10,107 @@
 *****************************************************/
 
 //Create a prototype for the huffman object
-function huffClass(probability, name, value) {
+function huffClass(probability, name, value, parent) {
   this.probability = probability;
   this.name = name;
   this.value = value;
+  this.parent = parent;
 };
 
 //Global variables
 var huffArr;
+var huffString;
 var huffArrStore;
+
+function wordIdentifier(){
+  //Get input String
+  var huffString = document.getElementById("huffString").value;
+
+  /* returns the size/length of an object */
+  Object.size = function(obj) {
+  	var size = 0;
+  	for(key in obj) {
+  		if(obj.hasOwnProperty(key)) size++;
+  	}
+  	return size;
+  }
+
+  //initial vars
+  huffLetters = new Object;
+
+  //loop, figure it out
+  for(x = 0, length = huffString.length; x < length; x++) {
+  	var l = huffString.charAt(x)
+  	huffLetters[l] = (isNaN(huffLetters[l]) ? 1 : huffLetters[l] + 1);
+  }
+
+  //output count!
+  for(key in letters) {
+  	console.log(key + ' :: ' + letters[key]);
+  }
+  console.log(Object.size(letters));
+}
 
 function displayTree(){
 
   //Display the output to html
-  var output = "Calculation : <br>";
+  //var output = "Calculation : <br>";
 
-  for (huffarrItr of huffArrStore) {
-    for (huffEle of huffarrItr) {
-      output += huffEle.probability + "&nbsp;&nbsp;&nbsp;";
-    }
-    output += "<br>"
+  // for (huffarrItr of huffArrStore) {
+  //   for (huffEle of huffarrItr) {
+  //     output += huffEle.probability + "&nbsp;&nbsp;&nbsp;";
+  //   }
+  //   output += "<br>"
+  // }
+
+  //document.getElementById("output").innerHTML = output;
+
+  var outputDiv = document.getElementById("output");
+
+  var heading = document.createElement("h3");
+  heading.className = "heading";
+  heading.innerHTML = "Calculations";
+  outputDiv.appendChild(heading);
+
+  var colSpan = document.createElement("span");
+  colSpan.className = "column";
+  outputDiv.appendChild(colSpan);
+
+  for(huffEle of huffArrStore[0]){
+    var cell = document.createElement("span");
+    cell.className = "cell-head";
+    cell.innerHTML = huffEle.name ;
+    colSpan.appendChild(cell);
   }
 
-  document.getElementById("output").innerHTML = output;
+  for (huffarrItr of huffArrStore) {
+    var colSpan = document.createElement("span");
+    colSpan.className = "column";
+    outputDiv.appendChild(colSpan);
+
+    for(huffEle of huffarrItr){
+      var cell = document.createElement("span");
+      cell.className = "cell"
+      cell.innerHTML = huffEle.probability ;
+      colSpan.appendChild(cell);
+    }
+  }
 
 }
 
 function calcCode(){
 
-  //todo [parse the tree]
-  var codes = "Codes : <br>";
+  //[parse the tree]
+  var codeDiv = document.getElementById("codeDiv");
+
+  var heading = document.createElement("h3");
+  heading.className = "heading";
+  heading.innerHTML = "Codes";
+  codeDiv.appendChild(heading);
+
+  var codepara = document.createElement("p");
+
+  var codes = "";
 
   for(var huffEle of huffArrStore[0]){
       var curCodeName = huffEle.name;
@@ -57,7 +128,8 @@ function calcCode(){
       codes += "<br>";
   }
 
-  document.getElementById("codes").innerHTML = codes;
+  codepara.innerHTML = codes;
+  codeDiv.appendChild(codepara);
 
 }
 
@@ -71,12 +143,22 @@ function testProbability(){
 }
 
 function huffLogic(){
-  //Get input String
-  var huffString = document.getElementById("huffString").value;
 
   //Initialise the arrays
-  huffArr = [];
   huffArrStore = [];
+  huffArr = [];
+
+  huffString = document.getElementById("huffString").value;
+
+  var outputDiv = document.getElementById("output");
+  while (outputDiv.hasChildNodes()) {
+    outputDiv.removeChild(outputDiv.lastChild);
+  }
+
+  var codeDiv = document.getElementById("codeDiv");
+  while (codeDiv.hasChildNodes()) {
+    codeDiv.removeChild(codeDiv.lastChild);
+  }
 
   //Split string to array
   huffArr = huffString.split(",").map(Number);
@@ -84,8 +166,8 @@ function huffLogic(){
 
   if(testProbability() != 100){
     var err_message = "Probabilites don't add up to 100";
-    document.getElementById('output').innerHTML = err_message;
-    document.getElementById('codes').innerHTML = "";
+    //document.getElementById('output').innerHTML = ;
+    document.getElementById('codeDiv').innerHTML = err_message;
     return;
   }
 
@@ -95,6 +177,7 @@ function huffLogic(){
     var huffobj = new huffClass(
       huffArr[i],
       'x' + i,
+      -1,
       -1
     );
     huffArrStore[0][i] = huffobj;
